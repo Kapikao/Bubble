bb43f39dac2da926d7223397a73c17d23e6f18b0
zosiur <152406718+zosiur@users.noreply.github.com>
2025-01-20 16:06:46 +0100

    quit itp
    drtguuytdruhtdrgeytr

--- Assets/Mainmenuu.cs
+++ Assets/Mainmenuu.cs
@@ -6,8 +6,12 @@ public class Mainmenuu : MonoBehaviour
   public void PlayGame()
    {
         SceneManager.LoadSceneAsync(2);
-
-
-
+        
    }
+  public void QuitGame()
+    {
+        Application.Quit();
+        
+    }
 }
+
