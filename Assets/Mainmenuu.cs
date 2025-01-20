514efaa54a7445bb7a0857b9f1d745f61231d6c4
zosiur <152406718+zosiur@users.noreply.github.com>
2025-01-20 15:38:43 +0100

    gui

---
+++ Assets/Mainmenuu.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Mainmenuu : MonoBehaviour
+{
+  public void PlayGame()
+   {
+        SceneManager.LoadSceneAsync(2);
+
+
+
+   }
+}
