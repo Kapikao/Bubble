a12bfab2115d38c19d1dda300b77b2177ed04f69
apisyt <86847964+apisyt@users.noreply.github.com>
2025-01-22 08:50:47 +0100

    Działawoowowowow

--- Assets/Scripts/Outofmap.cs
+++ Assets/Scripts/Outofmap.cs
@@ -1,15 +1,25 @@
 using UnityEngine;
 
-public class ChangePositionOnTrigger : MonoBehaviour
+public class Outofmap : MonoBehaviour
 {
-    // Ta metoda zostanie wywo?ana, gdy obiekt z tym skryptem wejdzie w obszar triggera
+    public int positionchose = 0; // Wybór pozycji
+    public Vector3 customSpawnPoint = new Vector3(0f, 8.51f, 0f); // Wspó³rzêdne niestandardowe (domyœlne)
+
     private void OnTriggerEnter2D(Collider2D other)
     {
-        // Sprawdzamy, czy obiekt to gracz (przyk?ad: Player tag)
+        // Sprawdzamy, czy obiekt to gracz (przyk³ad: Player tag)
         if (other.CompareTag("Player"))
         {
-            // Zmiana pozycji gracza na (0, 0, 0)
-            other.transform.position = Vector3.zero;
+            if (positionchose == 0)
+            {
+                // Odradzanie na pozycji (0,0,0)
+                other.transform.position = Vector3.zero;
+            }
+            else
+            {
+                // Odradzanie na niestandardowych wspó³rzêdnych
+                other.transform.position = customSpawnPoint;
+            }
         }
     }
 }
